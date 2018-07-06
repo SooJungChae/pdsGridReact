3d7276605233d7f2ff6a0aa062c193c6e77e5cc6
SooJungChae <naanace@gmail.com>
2018-07-06 18:03:00 +0900

    Add express data

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
+    state = {
+        response: ''
+    };
+
+    componentDidMount() {
+        this.callApi()
+            .then(res => this.setState({ response: res.express }))
+            .catch(err => console.log(err));
+    }
+
+    callApi = async() => {
+        const response = await fetch('/api/hello');
+        const body = await response.json();
+
+        if (response.status !== 200) throw Error(body.message);
+        return body;
+    }
+
     render() {
         return (
-            <TodoListTemplate form={<Form/>}>
-                <TodoItemList/>
-            </TodoListTemplate>
+            <div>
+                <TodoListTemplate form={<Form/>}>
+                    <TodoItemList/>
+                </TodoListTemplate>
+                <p>{this.state.response}</p>
+            </div>
         );
     }
 }
